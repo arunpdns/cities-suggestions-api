93d70292d899079185acb86eec3900e2aa6ca24d
arunpdns <101435201+arunpdns@users.noreply.github.com>
2022-03-12 05:12:16 +0530

    Update Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -11,8 +11,8 @@ import rateLimit from 'express-rate-limit';
 
 
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 15 minutes
-	max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 10, // Limit each IP to 10 requests per `window` (here, per 1 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
